01b4ac15e74e10bf86f9f02a0f023d476496247d
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-03-26 23:07:33 +0800

    feat: Querying data with useQuery

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,34 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__";
 
+const TRACKS = gql(`
+  query GetTracks {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+`);
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading";
+  if (error) return `Error! ${error.message}`;
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
